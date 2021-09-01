ee79206285485c2cbb6a5612ded1ec67aa93c7e9
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-09-01 12:14:25 +0200

    init

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -8,6 +8,7 @@ export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Je suis en vacances </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
